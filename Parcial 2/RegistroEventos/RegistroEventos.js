d88de00441859de1ae8de0036e6217a2b417c472
AndyIbarraVG <AndyIbarraVG@gmail.com>
2022-10-13 21:18:59 -0500

    Se agrego la carpeta Parcial 2 y sus respectivas carpetas

---
+++ Parcial 2/RegistroEventos/RegistroEventos.js
@@ -0,0 +1,5 @@
+function Caja1(){alert("Click en caja 1")};
+
+document.querySelector("#caja2").addEventListener("click", function(){alert("Click en caja 2")});
+
+document.getElementById("caja3").addEventListener("click", function(){alert("Click en caja 3")});
