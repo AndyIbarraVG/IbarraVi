26478bbafa480f940509af835aadd2d97e37db84
AndyIbarraVG <AndyIbarraVG@gmail.com>
2022-11-19 21:37:10 -0600

    Se arreglo un error en los archivos

--- Parcial 3/FormularioResponsivo/dist/GetRegistro.php
+++ Parcial 3/FormularioResponsivo/dist/GetRegistro.php
@@ -1,8 +1,11 @@
 <?php
-
 $registro["id"] = "2";
 $registro["nombre"] = "Andy";
-
+$registro["apellidopaterno"] = "Ibarra";
+$registro["apellidomaterno"] = "Villanueva";
+$registro["telefono"] = "8673414759";
+$registro["correo"] = "andyibarrazt210@gmail.com";
+$registro["contraseña"] = "Admin.123";
+$registro["repetircontraseña"] = "Admin.123";
 echo json_encode($registro);
-
 ?>
--- Parcial 3/FormularioResponsivo/src/index.js
+++ Parcial 3/FormularioResponsivo/src/index.js
@@ -1,12 +1,17 @@
 import Swal from "sweetalert2";
 
-const Enviar = document.querySelector("#btnEnviar")
+const Enviar = document.querySelector("#btnEnviar");
 
 Enviar.addEventListener("click", async() => {
     Swal.fire('Enviado','Se ha enviado con exito su formulario.','success')
-    
     let respuesta = await fetch('GetRegistro.php');
     let dato = await respuesta.json();
     document.getElementById("Id").value = dato.id
     document.getElementById("Nombre").value = dato.nombre
+    document.getElementById("ApPaterno").value = dato.apellidopaterno
+    document.getElementById("ApMaterno").value = dato.apellidomaterno
+    document.getElementById("Telefono").value = dato.telefono
+    document.getElementById("Correo").value = dato.correo
+    document.getElementById("Contraseña").value = dato.contraseña
+    document.getElementById("ConfContraseña").value = dato.repetircontraseña
 })
